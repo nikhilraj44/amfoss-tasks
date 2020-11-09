b401ad5cf5b60f248a8af276931acca7edbb6b0e
Nikhil Raj <56081753+nikhilraj44@users.noreply.github.com>
2020-11-09 23:20:44 +0530

    Add files via upload
    task 9

---
+++ task-9/spam.js
@@ -0,0 +1,15 @@
+var message = "Hi"; //spam message 
+
+var interval = 1  ; // in seconds 
+
+var count = 100; //send "Hi" 100 times
+
+var i = 0 ;
+//function to send message
+var timer = setInterval(function(){
+	document.getElementsByClassName('composer_rich_textarea')[0].innerHTML = message;
+	$('.im_submit').trigger('mousedown');	 
+	i++;
+	if( i  == count )
+	clearInterval(timer);
+} , interval * 1000 ) ;
